712ffb4b226b7ac7a7945e946b67d86aed74af42
RakhithaRR <rakhitharr@live.com>
2018-02-02 18:05:03 +0530

    Minor changes.

---
+++ Client/src/components/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <h1>Hello World</h1>
+    <h1>Hello World</h1>
+    <h1>Hello World</h1>
+    <h1>Hello World</h1>
+    <h1>Hello World</h1>
+    <h1>Hello World</h1>
+    <h1>Hello World</h1>
+    <h1>Hello World</h1>
+    <h1>Hello World</h1>
+    <h1>Hello World</h1>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+
