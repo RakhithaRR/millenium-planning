6721e386e69151d41d5ed76105de0e394548c675
RakhithaRR <rakhitharr@live.com>
2018-02-24 20:30:31 +0530

    Searching project updates.

--- Client/src/components/Projects/SearchProjects.vue
+++ Client/src/components/Projects/SearchProjects.vue
@@ -11,9 +11,16 @@
         ></v-text-field>
       </div>
     </v-flex>
-    <v-flex mt-2 md6 xs12 offset-md3>
-      <v-card >
-        <p>Hello</p>
+    <v-flex mt-2 md8 xs12 offset-md2>
+      <v-card hover tile v-for="(item,index) in projects" :key="`${index}`">
+        <v-card-title primary-title>
+          <div>
+            <h2>{{item.Name}}</h2>
+            <h4>{{item.Client}}</h4>
+          </div>
+
+        </v-card-title>
+        <p>{{item}}</p>
       </v-card>
     </v-flex>
   </v-content>
@@ -25,6 +32,7 @@
 
     data () {
       return {
+        projects: [],
 
       }
     },
@@ -32,8 +40,13 @@
       getProjects () {
         axios.get("http://localhost:3000/project/getProjects")
           .then((response) => {
-            console.log("wew");
             console.log(response.data);
+            for(var i in response.data){
+              if(response.data.hasOwnProperty(i)){
+                var obj = response.data[i]
+                this.projects.push(obj);
+              }
+            }
           })
           .catch((error) => {
             console.log(error);
